0b00c23b6c39d05d46cee32a5aac72840e9bd1a8
Sophia Kane <skane1391@gmail.com>
2025-07-17 00:59:47 -0600

    Creacion de los paquetes, estructuracion de las carpetas

---
+++ backend/src/test/java/daiquiri/de/mango/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package daiquiri.de.mango.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
